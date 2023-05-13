5de8b2e8b713be1c770cbdbd075935dcd7e6d830
edisontkp <im@edisontkp.com>
2023-05-13 05:28:13 +0000

    update and fix bugss

--- asymmetric.js
+++ asymmetric.js
@@ -1,5 +1,18 @@
 const {  publicEncrypt, privateDecrypt } = require('crypto');
-const { publicKey, privateKey } = require('./keypair');
+const { generateKeyPairSync } = require('crypto');
+
+// const { publicKey, privateKey } = require('./keypair');
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+  modulusLength: 2048, // the length of your key in bits
+  publicKeyEncoding: {
+    type: 'spki', // recommended to be 'spki' by the Node.js docs
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8', // recommended to be 'pkcs8' by the Node.js docs
+    format: 'pem',
+  },
+});
 
 
 const encryptedData = publicEncrypt(
--- signing.js
+++ signing.js
@@ -1,6 +1,19 @@
 const { createSign, createVerify } = require('crypto');
-const { publicKey, privateKey } = require('./keypair');
+const { generateKeyPairSync } = require('crypto');
 
+// const { publicKey, privateKey } = require('./keypair');
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+    modulusLength: 2048, // the length of your key in bits
+    publicKeyEncoding: {
+      type: 'spki', // recommended to be 'spki' by the Node.js docs
+      format: 'pem',
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8', // recommended to be 'pkcs8' by the Node.js docs
+      format: 'pem',
+    },
+  });
+  
 const data = 'this data must be signed';
 
 /// SIGN
