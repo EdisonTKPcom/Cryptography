173d18e976d97f4fd2e6ca1888b027353154f40c
edisontkp <im@edisontkp.com>
2023-05-13 12:21:59 +0800

    Create keypair.js

---
+++ keypair.js
@@ -0,0 +1,16 @@
+const { generateKeyPairSync } = require('crypto');
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+  modulusLength: 2048, // the length of your key in bits
+  publicKeyEncoding: {
+    type: 'spki', // recommended to be 'spki' by the Node.js docs
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8', // recommended to be 'pkcs8' by the Node.js docs
+    format: 'pem',
+  },
+});
+
+console.log(publicKey);
+console.log(privateKey);
